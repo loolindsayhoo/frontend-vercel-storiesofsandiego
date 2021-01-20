8c9059204400386e4bbaada4620ad6f376ce7749
loolindsayhoo <hoodlindsaym@gmail.com>
2021-01-20 11:48:26 -0700

    new backend constant

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,9 @@
 
+// Local Host backend for development
 // export const API_URL = "http://localhost:3000"
-export const API_URL = "https://stories-san-diego-backend.herokuapp.com/"
+
+// Osha's Test heroku version backend: (hosted under Osha heroku account)
+// export const API_URL = "https://stories-san-diego-backend.herokuapp.com/"
+
+// Lindsay's heroku version backend:
+export const API_URL = "https://backend-stories-san-diego.herokuapp.com/"
